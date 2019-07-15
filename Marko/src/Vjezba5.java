aaf578acaf7fa8f4b578021c5ed189818b934965
Marko <markosolarcro@gmail.com>
2019-07-15 21:47:57 +0200

    Dodan gitiginore

---
+++ Marko/src/Vjezba5.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Vjezba5 {
+
+	public static void main(String[] args) {
+		// Uèitati dva broja. Ako je jedan veæi od nule ispisati 
+		//"Barem je jedan broj veæi od 0" inaèe ispisati sumu oba broja.
+		
+	}
+
+}
